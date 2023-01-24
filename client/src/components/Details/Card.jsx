68cf682d10f192776602b96126850a07dae96a1c
Fannci <donot322bm@gmail.com>
2023-01-23 22:37:25 -0500

    /** ----- 322 -----*/

--- client/src/components/Details/Card.jsx
+++ client/src/components/Details/Card.jsx
@@ -2,35 +2,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 /** Import styles */
-// import css from './Card.module.css';
+import css from "./Card.module.css";
 
 const Card = ({ id, image, name, temperaments, weight_min, weight_max }) => {
+	const weight = `${weight_min} - ${weight_max} Kg`;
+	/* const temp = !temperaments?.split(",").map((el) => el.trim()); */
 	return (
-		<div>
-			<div>
-				<Link to={`/dogs/${id}`}>
-					<img src={image} alt="---322---" width="200px" />
-				</Link>
+		<div className={css.card}>
+			<div className={css.top_card}>
+				<div className={css.img}>
+					<Link to={`/dogs/${id}`}>
+						<img
+							className={css.image}
+							src={image}
+							alt={`${name} img`}
+							width="200px"
+						/>
+					</Link>
+				</div>
+				<h2 className={css.name}>{name}</h2>
 			</div>
-			<div>
-				<p>
-					<b>Name: </b>
-					<span>{name}</span>
-				</p>
-			</div>
-			<div>
-				<p>
-					<b>Temperaments: </b>
-					{temperaments}
-				</p>
-			</div>
-			<div>
-				<p>
-					<b>weight: </b>
-					<span>
-						{weight_min} - {weight_max}
-					</span>
-				</p>
+
+			<div className={css.bottom_card}>
+				<div className={css.temperaments}>
+					<span>{temperaments}</span>
+				</div>
+				<p className={css.weight}>{weight}</p>
 			</div>
 		</div>
 	);
