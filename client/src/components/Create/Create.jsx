11065e55f1476eb55414fab42dbd44752ece3bb4
Fannci <donot322bm@gmail.com>
2023-01-24 23:27:54 -0500

    /** ----- 322 -----*/

--- client/src/components/Create/Create.jsx
+++ client/src/components/Create/Create.jsx
@@ -1,26 +1,31 @@
 /** Import packages */
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 /** Import files */
 import { createData, getTemperaments } from "../../redux/actions";
 /** Import styles */
-// import css from './Create.module.css';
+import css from "./Create.module.css";
 
-const validate = (form) => {
+const validate = (data) => {
 	const errors = {};
-	if (!form.name) {
-		errors.name = "Name is required, it should not contain numbers";
+	if (!data.name) errors.name = "You must enter a name";
+	if (data.name.length < 4) errors.name = "Minimum 4 characters.";
+	if (!data.height_min) errors.height_min = "You must enter a minimum height";
+	if (!data.height_max) errors.height_max = "You must enter a maximum height";
+	if (!data.weight_min) errors.weight_min = "You must enter a minimum weight";
+	if (!data.weight_max) errors.weight_max = "You must enter a maximum weight";
+	if (!data.life_span) errors.life_span = "You must enter a life span";
+	if (data.weight_min > data.weight_max || data.weight_max < data.weight_min)
+		errors.weight_min =
+			"Minimum weight must be less or equal than maximum weight ";
+	if (data.height_min > data.height_max || data.height_max < data.height_min)
+		errors.height_min =
+			"Minimum height must be less or equal than maximum height ";
+	if (!data.temperaments) {
+		errors.temperaments = "You must select a temperament or create a new one";
 	}
-	if (!form.height_min || !form.height_max) {
-		errors.height = "Height is required";
-	}
-	if (!form.weight_min || !form.weight_max) {
-		errors.weight = "Weight is required";
-	}
-	if (!form.life_span) {
-		errors.life_span =
-			"Lifespan is required, type only numbers separated by a dash (-)";
+	if (!data.image) {
+		errors.image = "Insert a valid image";
 	}
 	return errors;
 };
@@ -32,10 +37,10 @@ const Create = () => {
 	const [selectedValue, setSelectedValue] = useState("Temperaments");
 	const [errors, setErrors] = useState({
 		name: "",
-		height_min: "",
-		height_max: "",
-		weight_min: "",
-		weight_max: "",
+		min_height: "",
+		max_height: "",
+		min_weight: "",
+		max_weight: "",
 		life_span: "",
 		image: "",
 	});
@@ -50,50 +55,17 @@ const Create = () => {
 		temperaments: [],
 	});
 
-	useEffect(() => {
-		dispatch(getTemperaments());
-	}, [dispatch]);
-
-	useEffect(() => {
-		if (
-			form.name.length > 0 &&
-			form.height_min.length > 0 &&
-			form.height_max.length > 0 &&
-			form.weight_min.length > 0 &&
-			form.weight_max.length > 0
-		)
-			setButton(false);
-		else setButton(true);
-	}, [form, setButton]);
-
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		dispatch(createData(form));
-		alert("The new dog was added successfully");
-		setForm({
-			name: "",
-			height_min: "",
-			height_max: "",
-			weight_min: "",
-			weight_max: "",
-			life_span: "",
-			image: "",
-			temperaments: [],
-		});
-	};
-
 	const handleChange = (event) => {
 		const { name, value } = event.target;
 		setForm({
 			...form,
 			[name]: value,
 		});
-		setErrors(
-			validate({
-				...form,
-				[name]: value,
-			})
-		);
+		const fieldErrors = validate({
+			...form,
+			[name]: value,
+		});
+		setErrors((prevErrors) => ({ ...prevErrors, [name]: fieldErrors[name] }));
 	};
 
 	const handleSelect = (event) => {
@@ -111,27 +83,89 @@ const Create = () => {
 			temperaments: form.temperaments.filter((temp) => temp !== item),
 		});
 	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		dispatch(createData(form));
+		alert("The new dog was added successfully");
+		setForm({
+			name: "",
+			height_min: "",
+			height_max: "",
+			weight_min: "",
+			weight_max: "",
+			life_span: "",
+			image: "",
+			temperaments: [],
+		});
+	};
+
+	useEffect(() => {
+		dispatch(getTemperaments());
+	}, [dispatch]);
+
+	useEffect(() => {
+		if (
+			form.name.length > 0 &&
+			form.height_min.length > 0 &&
+			form.height_max.length > 0 &&
+			form.weight_min.length > 0 &&
+			form.weight_max.length > 0
+		)
+			setButton(false);
+		else setButton(true);
+	}, [form, setButton]);
+
 	return (
-		<div>
-			<div>
-				<Link to="/home">
-					<button>Go home</button>
-				</Link>
-				<form action="" id="form" onSubmit={handleSubmit}>
-					<div>
-						<input
-							type="text"
-							value={form.name}
-							name="name"
-							onChange={(event) => handleChange(event)}
-							autoComplete="off"
-							placeholder="Name..."
-						/>
-					</div>
-					<div>{errors.name && <p>{errors.name}</p>}</div>{" "}
-					{/*mesaje ed error de nombre*/}
-					<div>
-						<div>
+		<form action="" id="form" onSubmit={handleSubmit} className={css.container}>
+			<div className={css.left_container}>
+				<div className={css.dog}></div>
+				<div className={css.content}>
+					<input
+						type="text"
+						value={form.name}
+						name="name"
+						required="required"
+						onChange={(event) => handleChange(event)}
+						autoComplete="off"
+					/>
+					<label htmlFor="name">Name:</label>
+					{errors.name && <div className={css.err}>{errors.name}</div>}
+				</div>
+
+				<div className={css.content}>
+					<input
+						type="text"
+						value={form.image}
+						name="image"
+						required="required"
+						onChange={(event) => handleChange(event)}
+						autoComplete="off"
+					/>
+					<label htmlFor="image">Image:</label>
+					{errors.image && <div className={css.err}>{errors.image}</div>}
+				</div>
+
+				<div className={css.content}>
+					<input
+						type="text"
+						autoComplete="off"
+						name="life_span"
+						required="required"
+						value={form.life_span}
+						onChange={(event) => handleChange(event)}
+					/>
+					<label htmlFor="life_span">Life: </label>
+					{errors.life_span && (
+						<div className={css.err}>{errors.life_span}</div>
+					)}
+				</div>
+			</div>
+
+			<div className={css.right_continer}>
+				<div className={css.top_content}>
+					<div className={css.left_top}>
+						<div className={css.label_input}>
 							<label htmlFor="height_min">Min Height </label>
 							<input
 								type="text"
@@ -140,23 +174,12 @@ const Create = () => {
 								placeholder="Min height..."
 								onChange={(event) => handleChange(event)}
 							/>
+							{errors.height_min && (
+								<div className={css.err}>{errors.height_min}</div>
+							)}
 						</div>
 
-						<div>
-							<label htmlFor="height_max">Max Height </label>
-							<input
-								type="text"
-								value={form.height_max}
-								name="height_max"
-								placeholder="Max height..."
-								onChange={(event) => handleChange(event)}
-							/>
-						</div>
-					</div>
-					<div>{errors.height && <p>{errors.height}</p>}</div>
-					{/* espacio para agregar error */}
-					<div>
-						<div>
+						<div className={css.label_input}>
 							<label htmlFor="weight_min">Min Weight </label>
 							<input
 								type="text"
@@ -165,9 +188,28 @@ const Create = () => {
 								placeholder="Min weight..."
 								onChange={(event) => handleChange(event)}
 							/>
+							{errors.weight_min && (
+								<div className={css.err}>{errors.weight_min}</div>
+							)}
+						</div>
+					</div>
+
+					<div className={css.right_top}>
+						<div className={css.label_input}>
+							<label htmlFor="height_max">Max Height </label>
+							<input
+								type="text"
+								value={form.height_max}
+								name="height_max"
+								placeholder="Max height..."
+								onChange={(event) => handleChange(event)}
+							/>
+							{errors.height_max && (
+								<div className={css.err}>{errors.height_max}</div>
+							)}
 						</div>
 
-						<div>
+						<div className={css.label_input}>
 							<label htmlFor="weight_max">Max Weight </label>
 							<input
 								type="text"
@@ -176,70 +218,61 @@ const Create = () => {
 								placeholder="Max weight..."
 								onChange={(event) => handleChange(event)}
 							/>
+							{errors.weight_max && (
+								<div className={css.err}>{errors.weight_max}</div>
+							)}
 						</div>
 					</div>
-					<div>{errors.weight && <p>{errors.weight}</p>}</div>
-					{/* espacio para agregar error */}
-					<div>
-						<label htmlFor="life_span">life_span </label>
-						<input
-							type="text"
-							autoComplete="off"
-							name="life_span"
-							value={form.life_span}
-							placeholder="lifespan exam: 10 - 12"
-							onChange={(event) => handleChange(event)}
-						/>
-					</div>
-					<div>{errors.life_span && <p>{errors.life_span}</p>}</div>
-					{/* espacio para agregar error */}
-					<div className="image-container">
-						<input
-							type="text"
-							autoComplete="off"
-							value={form.image}
-							name="image"
-							placeholder="Image URL..."
-							onChange={(event) => handleChange(event)}
-						/>
-					</div>
-					<div>
-						<h3>Select Temperaments</h3>
-					</div>
-					<div>
-						<select
-							value={selectedValue}
-							onChange={(event) => handleSelect(event)}>
-							<option disabled>Temperaments</option>
-							{temperaments.map((d) => (
-								<option value={d.name} key={d.name + Math.random()}>
-									{d.name}
-								</option>
+				</div>
+
+				<div className={css.bottom_content}>
+					<div className={css.temper}>
+						<div className={css.select}>
+							<p>
+								Temperaments:
+								<span style={{ color: "transparent" }}>_</span>
+							</p>
+							<select
+								value={selectedValue}
+								required
+								onChange={(event) => handleSelect(event)}>
+								<option disabled>Temperaments</option>
+								{temperaments.map((d) => (
+									<option value={d.name} key={d.name + Math.random()}>
+										{d.name}
+									</option>
+								))}
+							</select>
+						</div>
+
+						<div className={css.select_values}>
+							{form.temperaments.map((el) => (
+								<div
+									className={css.selected_values}
+									key={el}
+									onClick={() => handleDelete(el)}>
+									{`${el}`}
+								</div>
 							))}
-						</select>
+							{errors.temperaments && (
+								<div className={css.err}>{errors.temperaments}</div>
+							)}
+						</div>
 					</div>
-					<div>
-						<button disabled={button} type="submit" form="form">
-							Create Dog
-						</button>
-					</div>
-				</form>
-
-				<div className="">
-					<div className="">
-						<h2>Temperaments</h2>
-					</div>
-
-					<div>
-						{form.temperaments.map((el) => (
-							<div key={el} onClick={() => handleDelete(el)}>
-								<p>{`${el}`}</p>
-							</div>
-						))}
-					</div>
+					<button
+						disabled={button}
+						type="submit"
+						form="form"
+						className={css.button_send}>
+						<span>Create</span>
+						<svg viewBox="0 0 13 10" height="10px" width="15px">
+							<path d="M1,5 L11,5"></path>
+							<polyline points="8 1 12 5 8 9"></polyline>
+						</svg>
+					</button>
 				</div>
 			</div>
-		</div>
+		</form>
 	);
 };
 
